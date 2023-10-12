109801ab6298b497d7da3fad884298caaa567add
chanmrotzek <nerdtzek14@gmail.com>
2023-10-11 23:33:39 -0700

    cleaned up some stuff
    renamed/cleaned up some files to make their purposes clearer. pageStyle.css for the pages, backendScript.js for the backend, etc. plus, i added a neat lil index.html to pick between the demos.
    
    sorrrrry bridget 🥺 please review

---
+++ Front-End/backendScript.js
@@ -0,0 +1,14 @@
+//Test for backend functionality.
+var button = document.getElementById("button");
+
+//Test post on click.
+button.onclick = function() {
+    button.style.backgroundColor = "red";
+    const url = 'http://localhost:80/test';
+    const response = fetch(url, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: ''
+    });
+};
+
