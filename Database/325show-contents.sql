a49928ed374f6052508ab8a00899e2173158d6e6
Garrett Willis <122920316+Gwillis442@users.noreply.github.com>
2023-12-01 13:37:07 -0800

    select statements and output

---
+++ Database/325show-contents.sql
@@ -0,0 +1,71 @@
+/*
+DataBased
+----------------
+Garrett Willis
+Matthew MacDevitt
+Logan Bates
+Austin Wroblos
+Chan Mrotzek
+Bridget Acosta
+
+Last modified 12/1/23
+*/
+spool 325results-contents.txt
+
+prompt table customer
+prompt ====================
+select * 
+from customer;
+
+prompt table cust_order
+prompt ====================
+select * 
+from cust_order;
+
+prompt table subscription_order
+prompt ====================
+select * 
+from subscription_order;
+
+prompt table subscription_shipment
+prompt ====================
+select *
+from subscription_shipment;
+
+prompt table credit_card_info
+prompt ====================
+select * 
+from credit_card_info;
+
+prompt table one_time_order
+prompt ====================
+select *
+from one_time_order;
+
+prompt table product
+prompt ====================
+select *
+from product;
+
+prompt table order_product
+prompt ====================
+select * 
+from order_product;
+
+prompt table address
+prompt ====================
+select * 
+from address;
+
+prompt table account
+prompt ====================
+select * 
+from account;
+
+prompt table shipment
+prompt ====================
+select *
+from shipment;
+
+
+spool off
